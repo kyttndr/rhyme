ddb33fece8de36cc2c6135bdbde3b7b39aff162d
Peiyuan Sun <peiyuan.sun@mail.utoronto.ca>
2020-06-22 22:21:15 -0400

    new config

--- rhyme/settings.py
+++ rhyme/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/opt/rhyme/static/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR,]
